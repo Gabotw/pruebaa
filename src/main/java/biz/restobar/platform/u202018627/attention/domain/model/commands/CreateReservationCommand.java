ae60f880ef546fc1e550d7d525aa7091ffc39f88
Gabotw <u202018627@upc.edu.pe>
2024-11-05 19:02:15 -0500

    xd

--- src/main/java/biz/restobar/platform/u202018627/attention/domain/model/commands/CreateReservationCommand.java
+++ src/main/java/biz/restobar/platform/u202018627/attention/domain/model/commands/CreateReservationCommand.java
@@ -1,7 +1,5 @@
 package biz.restobar.platform.u202018627.attention.domain.model.commands;
 
-import biz.restobar.platform.u202018627.attention.domain.model.entities.Client;
-
 import java.util.Date;
 
 public record CreateReservationCommand(
--- src/main/java/biz/restobar/platform/u202018627/shared/interfaces/rest/resources/MessageResource.java
+++ src/main/java/biz/restobar/platform/u202018627/shared/interfaces/rest/resources/MessageResource.java
@@ -1,7 +1,4 @@
 package biz.restobar.platform.u202018627.shared.interfaces.rest.resources;
 
-/**
- * Message Resource
- */
 public record MessageResource(String message) {
 }
